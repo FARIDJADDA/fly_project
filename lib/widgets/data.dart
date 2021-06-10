34c9a2b1b9732f44d65c5f84f2b5e1cb6f01c2f7
Djadda Farid <faridjadda750@gmail.com>
2021-06-10 18:48:46 +0200

    ad methode inheritedWidget

---
+++ lib/widgets/data.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import '../models/activity.model.dart';
+import '../datas/data.dart' as data;
+
+class Data extends InheritedWidget {
+  final List<Activity> activities = data.activities;
+
+  Data({Widget child}) : super(child: child);
+
+  @override
+  bool updateShouldNotify(InheritedWidget oldWidget) {
+    throw true;
+  }
+
+  static of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<Data>();
+  }
+}
